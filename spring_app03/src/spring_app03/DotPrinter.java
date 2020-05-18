6ae0df738b776ca2697aecbf7f0d4108047b3b87
goott7-8 <goott7-8@DESKTOP-99L19FI>
2020-05-18 10:48:45 +0900

    Revert "spring 05/18 : 1"
    This reverts commit 2cded805018109325f178dab8016f6cf6e6bb06c.

--- spring_app03/src/spring_app03/DotPrinter.java
+++
@@ -1,9 +0,0 @@
-package spring_app03;
-
-public class DotPrinter implements Printer {
-	@Override
-	public void print(String msg) {
-		System.out.println("Dot Printer : 따다라다다다ㅏ닫ㄱ~~~");
-		System.out.println(msg);
-	}
-}
--- spring_app03/src/spring_app03/Printer.java
+++
@@ -1,7 +0,0 @@
-package spring_app03;
-
-public interface Printer {
-	
-	public void print(String msg);
-	
-}
