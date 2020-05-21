cc7315e9ab26072a0c8cfa9a560b81b1678192a9
goott7-8 <goott7-8@DESKTOP-99L19FI>
2020-05-21 14:44:16 +0900

    파일에 저장해서 출력해보세요, throw

--- spring_aop03_before_hw/src/advice/MethodAfter_Advice.java
+++ spring_aop03_before_hw/src/advice/MethodAfter_Advice.java
@@ -1,6 +1,9 @@
 package advice;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -36,6 +39,28 @@ public class MethodAfter_Advice implements AfterReturningAdvice {
 		if(!f2.exists()) {
 			f2.createNewFile();
 		};
+		
+		// 추가로 파일에 저장해서 출력해보세요
+		// '파일이름' 저장되었습니다.
+		// '10000' 이체되었습니다.
+		
+		 // 기존 파일의 내용에 이어서 쓰려면 true를, 기존 내용을 없애고 새로 쓰려면 false를 지정한다.
+
+		
+//		BufferedWriter writer = new BufferedWriter(new FileWriter(f2, true));
+		PrintWriter pw = new PrintWriter(f2);
+		
+		pw.write(fileName+" 저장되었습니다");
+		
+		pw.write(args[0]+" 이체되었습니다.");
+		pw.flush();
+		
+//		writer.write(fileName+" 저장되었습니다");
+//		writer.newLine();
+//		writer.write(args[0]+" 이체되었습니다.");
+//		writer.flush();
+		
+		// 이런식으로 생성하는 것이 로그 기록의 일정 => 감사자료....
 		
 		
 		// 매개변수 출력
