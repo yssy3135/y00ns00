077c99738a42be57d55f025b71836ae3776a01bf
goott7-8 <goott7-8@DESKTOP-99L19FI>
2020-05-25 16:40:29 +0900

    스프링 web_0523 hw

---
+++ spring_web03_hw/WebContent/jsp/nowTime.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>nowTime.jsp</title>
+</head>
+<body>
+	<h2>현재시각 : ${time }</h2>
+</body>
+</html>
---
+++ spring_web03_hw/src/spring_web03_hw/NowController.java
@@ -0,0 +1,32 @@
+package spring_web03_hw;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class NowController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		
+		Date d = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd - HH:mm:ss");
+		
+		String time = sdf.format(d);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("time",time);
+		mav.setViewName("nowTime");
+				
+		
+		
+		return mav;
+	}
+
+}
